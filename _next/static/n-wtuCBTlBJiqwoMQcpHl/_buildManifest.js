02cd6c0c71d705ab5af9a1cfa37d05334adf88db
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-11-12 23:31:08 +0530

    deployed next site

---
+++ _next/static/n-wtuCBTlBJiqwoMQcpHl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/695-a40a7b91816c37ca.js","static/css/b60938517f123559.css","static/chunks/pages/index-2a5036ddcdec2ea5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
